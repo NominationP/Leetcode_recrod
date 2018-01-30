e917352a11e31ee1ba8c2903857cc835ca98d0cf
NominationP <zhangze1029@gmail.com>
2018-01-30 23:31:54 +0800

    git-aut-commit

--- cmake-build-debug/CMakeFiles/Leetcode_recrod.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Leetcode_recrod.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Leetcode_recrod.dir/SwordOffer/Q/5_Capter/29_MoreThanHalfNumber/test.cpp.o"
+  "CMakeFiles/Leetcode_recrod.dir/SwordOffer/Q/5_Capter/30_KLeastNumbers/KLeastNumbers.cpp.o"
   "Leetcode_recrod.pdb"
   "Leetcode_recrod"
 )
